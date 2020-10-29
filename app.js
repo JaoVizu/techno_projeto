f21a183246fe565af1f2f2200769517898db8379
JaoVizu <joaovizu2015@gmail.com>
2020-10-29 10:42:30 -0300

    Controle de estoque

--- app.js
+++ app.js
@@ -73,6 +73,10 @@ const vm = new Vue({
       if(window.localStorage.carrinho)
         this.carrinho = JSON.parse(window.localStorage.carrinho)
     },
+    compararEstoque(){
+      const items = this.carrinho.filter(({id}) => id === this.produto.id)
+      this.produto.estoque -= items.length
+    },
     alerta(mensagem) {
       this.mensagemAlerta = mensagem
       this.alertaAtivo = true
@@ -91,6 +95,9 @@ const vm = new Vue({
       document.title = this.produto.nome || "Techno";
       const hash = this.produto.id || "";
       history.pushState(null, null, `#${hash}`);
+      if(this.produto){
+        this.compararEstoque()
+      }
     },
     carrinho() {
       window.localStorage.carrinho = JSON.stringify(this.carrinho)
